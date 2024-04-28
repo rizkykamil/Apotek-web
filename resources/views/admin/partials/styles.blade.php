602051847db0380e85598b3f81a4006c844fad74
rizkykamil <rabbaniikamil@gmail.com>
2024-04-28 16:37:18 +0700

    Remove unused Twitter Bootstrap CSS file from admin styles

--- resources/views/admin/partials/styles.blade.php
+++ resources/views/admin/partials/styles.blade.php
@@ -3,4 +3,3 @@
 <script data-search-pseudo-elements="" defer="" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
 <link src="https://cdn.datatables.net/2.0.5/css/dataTables.bootstrap5.css"></link>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
