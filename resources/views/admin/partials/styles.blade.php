b7361fb15b4d667215683a64e44138bb3c0afe2e
rizkykamil <rabbaniikamil@gmail.com>
2024-04-28 15:53:45 +0700

    fix tampilan history table

--- resources/views/admin/partials/styles.blade.php
+++ resources/views/admin/partials/styles.blade.php
@@ -2,5 +2,5 @@
 <link rel="icon" type="image/x-icon" href="{{asset("assets/img/favicon.png")}}" />
 <script data-search-pseudo-elements="" defer="" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/2.0.5/css/dataTables.bootstrap5.css"></link>
+<link src="https://cdn.datatables.net/2.0.5/css/dataTables.bootstrap5.css"></link>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
