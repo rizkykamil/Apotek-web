f4090aa6b56bea3e9edab79ecdc615b0c246b044
rizkykamil <rabbaniikamil@gmail.com>
2024-05-03 16:22:24 +0700

    Remove unnecessary Bootstrap bundle script from admin partials scripts

--- resources/views/admin/partials/scripts.blade.php
+++ resources/views/admin/partials/scripts.blade.php
@@ -11,7 +11,6 @@
 <script src="https://assets.startbootstrap.com/js/sb-customizer.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/2.0.5/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.0.5/js/dataTables.bootstrap5.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
